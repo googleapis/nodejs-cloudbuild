c67ded7f76390a6d784e207f7d3e0e2283e3d13e
yoshi-automation <yoshi-automation@google.com>
2019-11-12 04:11:50 -0800

    [CHANGE ME] Re-generated  to pick up changes in the API or client library generator.

---
+++ system-test/fixtures/sample/src/index.ts
@@ -0,0 +1,9 @@
+
+import {CloudBuildClient} from 'cloudbuild';
+
+function main() {
+  const cloudBuildClient = new CloudBuildClient();
+  console.log("cloudBuildClient was created!");
+}
+
+main();
